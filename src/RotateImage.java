3da3c4294df35656816a0200e0c2f86c0a2f9c6a
Devika-Sajeesh <devikasajeesh26@gmail.com>
2026-03-27 11:30:31 +0530

    fixed error

--- src/RotateImage.java
+++ src/RotateImage.java
@@ -38,7 +38,8 @@ public class RotateImage {
         // Print
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(mat[i][j] + " ");
+                System.out.print(mat[i][j]);
+                if (j < n - 1) System.out.print(" ");
             }
             System.out.println();
         }
