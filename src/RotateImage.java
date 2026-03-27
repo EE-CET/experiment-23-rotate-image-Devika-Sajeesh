cecf11499a0ab0efaf56957b85305abacea6950e
Devika-Sajeesh <devikasajeesh26@gmail.com>
2026-03-27 11:28:22 +0530

    fixed error

--- src/RotateImage.java
+++ src/RotateImage.java
@@ -1,9 +1,48 @@
 public class RotateImage {
     
-        // TODO: Read n and n (dimensions)
-        // TODO: Read the matrix elements
-        // TODO: Rotate the matrix 90 degrees clockwise INPLACE
-        // Hint: 1. Transpose the matrix. 2. Reverse each row.
-        // TODO: Print the rotated matrix
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        
+        int n = sc.nextInt();
+        // Skip the second integer if it's there (problem statement says "n and n")
+        // But usually input format might duplicate it or just provide one N for square matrix.
+        // Example input: "2 2", so we need to consume the second integer.
+        int n2 = sc.nextInt(); 
+        
+        int[][] mat = new int[n][n];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+        
+        // 1. Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+        
+        // 2. Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[i][n - 1 - j];
+                mat[i][n - 1 - j] = temp;
+            }
+        }
+        
+        // Print
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
     
 }
